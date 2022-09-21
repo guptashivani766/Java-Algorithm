7f4daf9e72404744187d9dfa8b77727f900fd93f
Shivani Gupta <91816364+guptashivani766@users.noreply.github.com>
2022-09-21 12:06:55 +0530

    uploaded

---
+++ src/com/programs/PairSum1.java
@@ -0,0 +1,34 @@
+package com.programs;
+
+import java.util.ArrayList;
+
+public class PairSum1 {
+
+	public static boolean pairSum(ArrayList<Integer> list,int target) {
+		int lp=0,rp=list.size()-1;
+		while(lp<rp) {
+			if(list.get(lp)+list.get(rp)==target) {
+				return true;
+			}
+			else if(list.get(lp)+list.get(rp)<target) {
+				lp++;
+			}
+			else {
+				rp--;
+			}
+		}
+		return false;
+	}
+	public static void main(String[] args) {
+		ArrayList<Integer> list=new ArrayList<>();
+		list.add(1);
+		list.add(2);
+		list.add(3);
+		list.add(4);
+		list.add(5);
+		int target=9;
+		System.out.println(pairSum(list,target));
+
+	}
+
+}
