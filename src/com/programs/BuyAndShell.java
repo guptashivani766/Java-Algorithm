5f662aad2ab3db5b8451bf78c5cbaafef2111f0f
Shivani Gupta <91816364+guptashivani766@users.noreply.github.com>
2022-09-22 16:36:08 +0530

    uploaded

---
+++ src/com/programs/BuyAndShell.java
@@ -0,0 +1,25 @@
+package com.programs;
+
+public class BuyAndShell {
+	public static int buyAndShellStocks(int [] prices) {
+		int maxProfit=0;
+		int buyPrice=Integer.MAX_VALUE;
+		for(int i=0;i<prices.length;i++) {
+			if(buyPrice<prices[i]) {
+				int profit=prices[i]-buyPrice;
+				maxProfit=Math.max(profit, maxProfit);
+			}
+			else {
+				buyPrice=prices[i];
+			}
+		}
+		return maxProfit;
+	}
+
+	public static void main(String[] args) {
+		int [] prices= {7,1,5,3,6,4};
+		System.out.println(buyAndShellStocks(prices));
+
+	}
+
+}
