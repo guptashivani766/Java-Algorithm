4b8d3cbc33163402af3185f7b06cefcdf6e4d2d1
Shivani Gupta <91816364+guptashivani766@users.noreply.github.com>
2022-09-29 19:20:12 +0530

    uploaded

---
+++ src/com/programs/SearchInBST.java
@@ -0,0 +1,63 @@
+package com.programs;
+
+public class SearchInBST {
+	 static class Node{
+		 int data;
+		 Node left;
+		 Node right;
+		 public Node(int data) {
+			 this.data = data;
+		 }
+	 }
+	 public static Node insert(Node root,int val) {
+		 if(root == null) {
+			 root =new Node(val);
+			 return root;
+		 }
+		 if(root.data >val) {
+			 root.left = insert(root.left,val);
+		 }
+		 else {
+			 root.right =insert(root.right,val);
+		 }
+		 return root;
+	 }
+	 public static void inOrder(Node root) {
+		 if(root == null) {
+			 return;
+		 }
+		 inOrder(root.left);
+		 System.out.print(root.data+"");
+		 inOrder(root.right);
+	 }
+
+	 public static boolean search(Node root,int key) {
+		 if(root ==null) {
+			 return false;
+		 }
+		 if(root.data == key) {
+			 return true;
+		 }
+		 if(root.data>key) {
+			 search(root.left,key);
+		 }
+		 else {
+			 search(root.right,key);
+		 }
+		 return false;
+	 }
+	public static void main(String[] args) {
+		int val[] = {5,1,3,4,2,7};
+		Node root =null;
+		for(int i=0;i<val.length;i++) {
+			root = insert(root,val[i]);
+		}
+		inOrder(root);
+		System.out.println();
+		System.out.print(search(root,5));
+		
+		
+		
+	}
+
+}
